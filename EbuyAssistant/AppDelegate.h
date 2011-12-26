ddc90a89dd6168e9b5c210e0dd13a9d0ba9565ee
fengjunqian <fengjunqian@iMac.local>
2011-12-26 09:59:47 +0800

    Initial Commit

---
+++ EbuyAssistant/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  EbuyAssistant
+//
+//  Created by C J on 12/26/11.
+//  Copyright (c) 2011 Youshang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
